e27722f7ccd47e0371eb36486bf408f4545b6886
Cheshulko <chehhmann.nick@gmail.com>
2023-04-10 20:25:01 +0300

    exercism.io/rust/macros

---
+++ exercism.io/rust/macros/src/lib.rs
@@ -0,0 +1,12 @@
+#[macro_export]
+macro_rules! hashmap {
+    () => {
+        ::std::collections::HashMap::new()
+    };
+    ($key:expr => $value:expr) => {
+        ::std::collections::HashMap::from([($key, $value)])
+    };
+    ($($key:expr => $value:expr),+ $(,)?) => {
+        ::std::collections::HashMap::from([$(($key, $value),)*])
+    };
+}
