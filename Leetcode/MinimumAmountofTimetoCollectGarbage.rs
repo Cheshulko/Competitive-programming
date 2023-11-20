143390751e962ecfa748bf81e0f921bf0321e4d6
Cheshulko <chehhmann.nick@gmail.com>
2023-11-20 16:17:47 +0200

    MinimumAmountofTimetoCollectGarbage.rs

---
+++ Leetcode/MinimumAmountofTimetoCollectGarbage.rs
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/minimum-amount-of-time-to-collect-garbage
+
+struct Solution {}
+
+impl Solution {
+    pub fn garbage_collection(garbage: Vec<String>, travel: Vec<i32>) -> i32 {
+        let n = travel.len();
+        let mut pref = vec![0; n + 2];
+
+        for (i, house) in travel.into_iter().enumerate() {
+            pref[i + 1] = pref[i] + house;
+        }
+
+        ['M', 'P', 'G'].into_iter().fold(0, |sum, t| {
+            sum + garbage
+                .iter()
+                .enumerate()
+                .fold((0, 0), |(mut sum, mut prev_stop), (ind, house)| {
+                    let count = house.chars().filter(|c| c == t).count() as i32;
+                    if count > 0 {
+                        sum += pref[ind] - pref[prev_stop];
+                        sum += count;
+                        prev_stop = ind;
+                    }
+                    println!("{ind} {sum} {prev_stop}");
+                    (sum, prev_stop)
+                })
+                .0
+        })
+    }
+}
