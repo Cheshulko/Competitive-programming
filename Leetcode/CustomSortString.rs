30f358f71f89c0b5f26e82ee8faf09e57e639fe2
Cheshulko <chehhmann.nick@gmail.com>
2024-03-11 11:00:58 +0200

    Fix CustomSortString.rs

--- Leetcode/CustomSortString.rs
+++ Leetcode/CustomSortString.rs
@@ -4,15 +4,12 @@ struct Solution {}
 
 impl Solution {
     pub fn custom_sort_string(order: String, s: String) -> String {
-        let order =
-            order
-                .as_bytes()
-                .iter()
-                .enumerate()
-                .fold(vec![usize::MAX; 150], |mut v, (i, c)| {
-                    v[*c as usize] = i;
-                    v
-                });
+        let order = order
+            .char_indices()
+            .fold(vec![usize::MAX; 150], |mut v, (i, c)| {
+                v[c as usize] = i;
+                v
+            });
 
         let mut s = s.chars().collect::<Vec<_>>();
         s.sort_by(|a, b| order[*a as usize].cmp(&order[*b as usize]));
