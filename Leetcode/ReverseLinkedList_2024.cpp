e4d2a7504899e588287f314d94d6d76a9975b741
Cheshulko <chehhmann.nick@gmail.com>
2024-03-21 16:17:32 +0200

    ReverseLinkedList_2024.cpp

---
+++ Leetcode/ReverseLinkedList_2024.cpp
@@ -0,0 +1,32 @@
+https :  // leetcode.com/problems/reverse-linked-list
+
+         /**
+          * Definition for singly-linked list.
+          * struct ListNode {
+          *     int val;
+          *     ListNode *next;
+          *     ListNode() : val(0), next(nullptr) {}
+          *     ListNode(int x) : val(x), next(nullptr) {}
+          *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+          * };
+          */
+
+         class Solution {
+   public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head) {
+            return nullptr;
+        }
+
+        auto next = head->next;
+        if (next) {
+            auto new_head = Solution::reverseList(next);
+            next->next = head;
+            head->next = nullptr;
+
+            return new_head;
+        } else {
+            return head;
+        }
+    }
+};
