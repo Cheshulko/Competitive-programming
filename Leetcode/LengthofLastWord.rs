cb6940f0c2efe3982f54383ea0a9f06f7859a192
Cheshulko <chehhmann.nick@gmail.com>
2024-04-01 16:20:05 +0300

    LengthofLastWord.rs

---
+++ Leetcode/LengthofLastWord.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/length-of-last-word
+
+struct Solution {}
+
+impl Solution {
+    pub fn length_of_last_word(s: String) -> i32 {
+        s.trim().split_whitespace().last().unwrap_or("").len() as i32
+    }
+}
