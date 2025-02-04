e6b70b59c3fd89312b4710d47dccaac44c3e6e5c
Cheshulko <chehhmann.nick@gmail.com>
2025-02-04 12:24:59 +0200

    Leetcode/MaximumAscendingSubarraySum.rs

---
+++ Leetcode/MaximumAscendingSubarraySum.rs
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/maximum-ascending-subarray-sum
+
+struct Solution {}
+
+impl Solution {
+    pub fn max_ascending_sum(nums: Vec<i32>) -> i32 {
+        let mut ans = nums[0];
+
+        let mut cur = nums[0];
+        for i in 1..nums.len() {
+            if nums[i - 1] < nums[i] {
+                cur += nums[i];
+            } else {
+                cur = nums[i];
+            }
+            ans = ans.max(cur);
+        }
+
+        ans
+    }
+}
