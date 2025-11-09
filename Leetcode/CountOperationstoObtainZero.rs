ca8234628ca787501fb808b75e7dff547f392dd8
Cheshulko <chehhmann.nick@gmail.com>
2025-11-09 12:29:25 +0200

    Leetcode/CountOperationstoObtainZero.rs

---
+++ Leetcode/CountOperationstoObtainZero.rs
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/count-operations-to-obtain-zero
+
+struct Solution {}
+
+impl Solution {
+    pub fn count_operations(mut num1: i32, mut num2: i32) -> i32 {
+        let mut ans = 0;
+
+        while num1 > 0 && num2 > 0 {
+            if num1 < num2 {
+                std::mem::swap(&mut num1, &mut num2);
+            }
+            ans += num1 / num2;
+            num1 %= num2;
+        }
+
+        ans
+    }
+}
