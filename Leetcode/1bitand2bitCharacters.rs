abaaae1a1a6c96dcd3f35fde8f33e7f6eaac8ed7
Cheshulko <chehhmann.nick@gmail.com>
2025-11-18 11:07:03 +0200

    Leetcode/1bitand2bitCharacters.rs

---
+++ Leetcode/1bitand2bitCharacters.rs
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/1-bit-and-2-bit-characters
+
+struct Solution {}
+
+impl Solution {
+    pub fn is_one_bit_character(bits: Vec<i32>) -> bool {
+        let n = bits.len();
+
+        let mut i = 0;
+        while i < n - 1 {
+            if bits[i] == 0 {
+                i += 1;
+            } else {
+                i += 2;
+            }
+        }
+
+        i == n - 1
+    }
+}
