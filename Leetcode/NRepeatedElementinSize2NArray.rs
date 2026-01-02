3b1f67186f65c814bacc3867863a38be63327115
Cheshulko <chehhmann.nick@gmail.com>
2026-01-02 11:51:07 +0200

    Leetcode/NRepeatedElementinSize2NArray.rs

---
+++ Leetcode/NRepeatedElementinSize2NArray.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/n-repeated-element-in-size-2n-array
+
+struct Solution {}
+
+impl Solution {
+    pub fn repeated_n_times(nums: Vec<i32>) -> i32 {
+        for i in 0..nums.len() - 2 {
+            if nums[i] == nums[i + 1] || nums[i] == nums[i + 2] {
+                return nums[i];
+            }
+        }
+
+        nums.last().copied().unwrap()
+    }
+}
