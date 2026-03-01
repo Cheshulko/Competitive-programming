4736a1b1189f3c1b600e5dce9564c8da42b4f64e
Cheshulko <chehhmann.nick@gmail.com>
2026-03-01 10:18:12 +0200

    Leetcode/PartitioningIntoMinimumNumberOfDeci-BinaryNumbers.rs

---
+++ Leetcode/PartitioningIntoMinimumNumberOfDeci-BinaryNumbers.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers
+
+struct Solution;
+
+impl Solution {
+    pub fn min_partitions(n: String) -> i32 {
+        (n.into_bytes().into_iter().max().unwrap() - b'0') as i32
+    }
+}
